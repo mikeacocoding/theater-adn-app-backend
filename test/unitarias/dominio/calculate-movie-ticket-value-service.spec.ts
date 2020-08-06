803a745d137df12b727c0c70a63510a43b2b0bc5
Miguel Acosta <dev.miguel.acosta@gmail.com>
2020-08-05 22:39:50 -0500

    Test unitario a Servicio de Cálculo de Costos

--- test/unitarias/dominio/calculate-movie-ticket-value-service.spec.ts
+++ test/unitarias/dominio/calculate-movie-ticket-value-service.spec.ts
@@ -1,7 +1,60 @@
+import { TestingModule, Test } from '@nestjs/testing';
+import { CalculateMovieTicketValueService } from 'src/domain/movie-ticket/services/CalculateMovieTicketValue.service';
+import MovieTicket from 'src/domain/movie-ticket/model/movie-ticket';
+import Movie from 'src/domain/movie/model/movie';
 
-describe('Calculate Movie Ticket Value Service', () => {
+describe('Hola Mundo TEST', () => {
   const text = 'Hola Mundo';
   it('Debe contener Mundo', () => {
     expect(text).toMatch(/Mundo/);
+  });
+});
+
+describe('Calculate Movie Ticket Value Service', () => {
+  let calculateService: CalculateMovieTicketValueService;
+  let movieTicket: MovieTicket;
+  let basePrice: number;
+
+  beforeAll(() => {
+    calculateService = new CalculateMovieTicketValueService();
+
+    basePrice = 20000;
+    const movie = new Movie('1', 'Start wars', 'Desc', 'url', basePrice);
+    movieTicket = new MovieTicket('1', 0, movie);
+
+    let testDate = new Date(2020, 7, 5, 9);
+    console.log(testDate);
+
+    movieTicket.setDate(testDate);
+    movieTicket.setTicketId(movieTicket.generateTicketId());
+  });
+
+  describe('Testing different Calculus', () => {
+    it('Expect not value modification', () => {
+      //Monday
+      movieTicket.getDate().setDate(3);
+      const value = calculateService.calculate(movieTicket);
+      expect(value).toBe(basePrice);
+    });
+
+    it('Expect a 10% higher value', () => {
+      //Saturday
+      movieTicket.getDate().setDate(8);
+      const value = calculateService.calculate(movieTicket);
+
+      let newValue = basePrice*(1 + 0.1);
+      newValue = parseInt(newValue.toFixed(0));
+      expect(value).toBe(newValue);
+    });
+
+    it('Expect a 20% lower value', () => {
+      //Wednesday
+      movieTicket.getDate().setDate(5);
+      const value = calculateService.calculate(movieTicket);
+
+      let newValue = basePrice*(1 - 0.2);
+      newValue = parseInt(newValue.toFixed(0));
+      expect(newValue).toBe(newValue);
+    });
   });
 });
