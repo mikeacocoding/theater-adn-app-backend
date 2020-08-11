bb2ff87ae18c7ac4255749c41fa152ce3a1fa0e0
Miguel Acosta <dev.miguel.acosta@gmail.com>
2020-08-11 10:45:59 -0500

    Cambio de puerto, Get Movie By Id Use Case implementado

---
+++ src/application/UseCases/movie/getMovieById.usecase.ts
@@ -0,0 +1,15 @@
+import { Injectable, Inject } from '@nestjs/common';
+import Movie from 'src/domain/movie/model/movie';
+import { MovieRepository } from 'src/domain/movie/ports/repository/movie.repository';
+import { Optional } from 'typescript-optional';
+
+@Injectable()
+export default class GetMoviebyIdUseCase {
+  constructor(
+    @Inject('MovieRepository') private movieRepository: MovieRepository,
+  ) {}
+
+  public handle(movieId: string): Promise<Optional<Movie>> {
+    return this.movieRepository.getById(movieId);
+  }
+}
